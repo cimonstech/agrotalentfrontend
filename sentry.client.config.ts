2460969d0925e3ef618fa36c7e03ecf391fdb447
nusely <batista.simons1@gmail.com>
2026-05-06 09:26:26 +0000

    update Sentry DSNs, add client config

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -4,6 +4,10 @@ Sentry.init({
   dsn: 'https://b60391457ffced9a807aac013260d89e@o4511339607359488.ingest.de.sentry.io/4511342146748496',
   environment: process.env.NODE_ENV ?? 'development',
   tracesSampleRate: process.env.NODE_ENV === 'production' ? 0.1 : 1,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
   enableLogs: true,
   sendDefaultPii: false,
+  integrations: [],
 })
+
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,19 +1,9 @@
-// This file configures the initialization of Sentry on the server.
-// The config you add here will be used whenever the server handles a request.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
-  dsn: "https://fc105aab189d76a10d059c20d4877d77@o4510815462817792.ingest.us.sentry.io/4510815472648192",
-
-  // Lower sample rate in production to reduce overhead (1 = 100%, 0.1 = 10%)
+  dsn: 'https://b60391457ffced9a807aac013260d89e@o4511339607359488.ingest.de.sentry.io/4511342146748496',
+  environment: process.env.NODE_ENV ?? 'development',
   tracesSampleRate: process.env.NODE_ENV === 'production' ? 0.1 : 1,
-
-  // Enable logs to be sent to Sentry
   enableLogs: true,
-
-  // Enable sending user PII (Personally Identifiable Information)
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
-  sendDefaultPii: true,
-});
+  sendDefaultPii: false,
+})
